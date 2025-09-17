8448bab2070b3549bcc1d87a17e3f226413e458e
npavani <npavani@pavani-sa.stratapps.com>
2025-09-17 14:44:13 +0530

    Second Commit

--- src/TestPkg/Test.java
+++ src/TestPkg/Test.java
@@ -4,7 +4,6 @@ public class Test {
 
 		public static void main(String[] args) {
 		System.out.println("Testing Git integration");
-		System.out.println("Testing Git integration - 2nd line");
-	    System.out.println("Testing Git integration - 3rd line");
+		System.out.println("Second Commit change");
 	}
 }
